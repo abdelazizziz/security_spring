6898f32020609deee40d743a577a5c33268f9425
abdelziz <abdelazizboushib@gmail.com>
2024-08-19 01:42:36 +0100

    genarte jwt to application v1

--- src/main/java/tech/chillo/avis/controleur/UtilisateurControleur.java
+++ src/main/java/tech/chillo/avis/controleur/UtilisateurControleur.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.chillo.avis.Dto.AuthenicationDTO;
 import tech.chillo.avis.entite.Utilisateur;
+import tech.chillo.avis.securite.JwtService;
 import tech.chillo.avis.service.UtilisateurService;
 
 import java.util.Map;
@@ -24,6 +25,7 @@ public class UtilisateurControleur {
 
     private UtilisateurService utilisateurService;
     private AuthenticationManager authenticationManager;
+    private JwtService jwtService;
 
     @PostMapping(path = "inscription")
     public void inscription(@RequestBody Utilisateur utilisateur) {
@@ -40,6 +42,9 @@ public class UtilisateurControleur {
       final   Authentication authenticate = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authenicationDTO.username(), authenicationDTO.password()));
       log.info("authentication is {}", authenticate.isAuthenticated());
+      if (authenticate.isAuthenticated()) {
+          return jwtService.generer(authenicationDTO.username());
+      }
         return null;
 
     }
---
+++ src/main/java/tech/chillo/avis/securite/JwtService.java
@@ -0,0 +1,54 @@
+package tech.chillo.avis.securite;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tech.chillo.avis.entite.Utilisateur;
+import tech.chillo.avis.service.UtilisateurService;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class JwtService {
+    private UtilisateurService utilisateurService;
+    private final String ENCRIPTION_KEY = "608f36e92dc66d97d5933f0e6371493cb4fc05b1aa8f8de64014732472303a7c";
+
+    public Map<String,String> generer(String email) {
+        Utilisateur utilisateur = (Utilisateur) this.utilisateurService.loadUserByUsername(email);
+        return this.generateJWT(utilisateur);
+    }
+
+    private Map<String, String> generateJWT(Utilisateur utilisateur) {
+
+        final long currentTime = System.currentTimeMillis();
+        final long expiration = currentTime+ 10 * 60 * 1000;
+        final Map<String, String> claims = Map.of(
+                "nom", utilisateur.getNom(),
+                "email", utilisateur.getEmail()
+
+        );
+
+        final String bearer = Jwts.builder()
+                .setIssuedAt(new Date(currentTime))
+                .setExpiration(new Date(expiration))
+                .setSubject(utilisateur.getEmail())
+                .setClaims(claims)
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+
+        return Map.of("bearer", bearer);
+    }
+
+    private Key getKey() {
+        final byte[] decoder = Decoders.BASE64.decode(ENCRIPTION_KEY);
+        return Keys.hmacShaKeyFor(decoder);
+    }
+
+}
